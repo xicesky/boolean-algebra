6a1d3396d5a15754601b78d0bc162a26dce37ab3
Markus Dangl <663399+xicesky@users.noreply.github.com>
2021-01-31 22:20:31 +0100

    F-Alg version of simplifyPrimitive, with notes

--- src/BooleanAlgebra.hs
+++ src/BooleanAlgebra.hs
@@ -5,17 +5,31 @@ module BooleanAlgebra
     , simplifyPrimitive
     , exampleExpr01
     , exampleExpr02
+
+    -- Temporary exports for playing in ghci
+    , Fix(..)
+    , Algebra
+    , Coalgebra
+    , cata
+    , ana
+    , simp
+
     ) where
 
 {- Inspiration:
     https://tuprints.ulb.tu-darmstadt.de/2759/1/rkibria-dissertation-final-korrigiert1.pdf
     https://bartoszmilewski.com/2017/02/28/f-algebras/
     https://www-ps.informatik.uni-kiel.de/~sebf/projects/sat-solver/Control/Monad/Constraint/Boolean.lhs.html
+    Recursion schemes: https://blog.sumtypeofway.com/archive.html
 -}
+
+import Data.Fix (Fix(..), foldFix, unfoldFix)
+import Data.Maybe (fromMaybe)
+import Control.Monad (liftM2)
 
 {-----------------------------------------------------------------------------}
 
-import Data.Fix (Fix(..), foldFix, unfoldFix)
+
 -- Fix :: f (Fix f) -> Fix f
 -- unFix :: Fix f -> f (Fix f)
 -- foldFix :: (f a -> a) -> Fix f -> a
@@ -30,6 +44,48 @@ cata = foldFix
 
 ana :: Functor f => Coalgebra f a -> a -> Fix f
 ana = unfoldFix
+
+{-----------------------------------------------------------------------------}
+-- Thinking
+
+-- type L2Algebra f a = f (f a) -> f a
+type L2Algebra f a = Algebra f (f a)
+
+l2cata :: Functor f => L2Algebra f a -> Fix f -> f a
+l2cata = cata
+-- This works perfectly when a = Fix f
+
+-- type Rewriter1 f = f (Fix f) -> Fix f
+type Rewriter1 f = Algebra f (Fix f)
+idRewriter1 :: Rewriter1 f
+idRewriter1 = Fix
+
+-- type Rewriter2 f = f (f (Fix f)) -> f (Fix f)
+type Rewriter2 f = Algebra f (f (Fix f))
+idRewriter2 :: Functor f => Rewriter2 f
+idRewriter2 = fmap Fix
+
+rw2cata :: Functor f => Rewriter2 f -> Fix f -> f (Fix f)
+rw2cata = l2cata
+
+-- and then there is the fucking partial function...
+type MbAlgebra f a = f a -> Maybe a
+-- We can't really do anything here, there is no way
+--  to get an a from f a in the "Nothing" case
+--  but there could be, if we have a "fallback" algebra:
+type OptAlgebra f a = (f a -> Maybe a, f a -> a)
+-- The second function will most likely just be "Fix", because it does nothing
+
+-- ?? a pair of functions is equal to a function that returns a pair
+-- ?? type OptAlgebra f a = f a -> (Maybe a, a)
+-- ?? This doens't look useful at first sight
+
+-- We could use something like this to resolve
+fallback :: (a -> b) -> (a -> Maybe b) -> a -> b
+-- This looks like the reader monad over a fromMaybe...
+fallback = liftM2 fromMaybe
+-- Or, implemented boringly:
+-- fallback f g x = fromMaybe (g x) (f x)
 
 {-----------------------------------------------------------------------------}
 
@@ -106,14 +162,7 @@ exampleExpr01 = (bNot (bVar "x" `bAnd` bVar "y")) `bAnd` bVar "z"
 
 {-----------------------------------------------------------------------------}
 
--- How to match on nested stuff
--- e.g. Match ((BFVal BTrue) `BFOr` e)
-stepOne :: BooleanExpr vn -> BooleanF vn (BooleanExpr vn)
-stepOne = unFix
-
-stepTwo :: BooleanF vn (BooleanExpr vn) -> BooleanF vn (BooleanF vn (BooleanExpr vn))
-stepTwo = fmap unFix
-
+-- ?? simp :: f (f a) -> Maybe (f a)
 simp :: BooleanF vn (BooleanF vn a) -> Maybe (BooleanF vn a)
 simp (BFNot (BFVal BTrue)) = Just (BFVal BFalse)
 simp (BFNot (BFVal BFalse)) = Just (BFVal BTrue)
@@ -127,108 +176,24 @@ simp (BFOr _ (BFVal BTrue)) = Just (BFVal BTrue)
 simp (BFOr e (BFVal BFalse)) = Just e
 simp _ = Nothing
 
-repeatF :: (a -> Maybe a) -> a -> a
-repeatF f x = case f x of
-    Just y  -> repeatF f y      -- apply again
-    Nothing -> x                --- done
-
--- with a = BooleanExpr vn
-repeatFB1 :: (BooleanExpr vn -> Maybe (BooleanExpr vn)) -> BooleanExpr vn -> BooleanExpr vn
-repeatFB1 f x = case f x of
-    Just y  -> repeatF f y      -- apply again
-    Nothing -> x                --- done
-
-
 simplifyPrimitive :: BooleanExpr vn -> BooleanExpr vn
-simplifyPrimitive e = let
-    -- cata :: (f a -> a) -> Fix f -> a 
-    --  with
-    --      f = BooleanF vn
-    --      a = BooleanF vn b
-    myCata1 :: (BooleanF vn (BooleanF vn b) -> (BooleanF vn b)) -> Fix (BooleanF vn) -> (BooleanF vn b)
-    myCata1 = cata
-    
-    --  with
-    --      b = BooleanExpr vn
-    myCata2 :: (BooleanF vn (BooleanF vn (BooleanExpr vn))
-        -> (BooleanF vn (BooleanExpr vn)))
-        -> Fix (BooleanF vn)                -- read: BooleanExpr vn
-        -> (BooleanF vn (BooleanExpr vn))
-    myCata2 = myCata1
-
-    -- simp :: BooleanF vn (BooleanF vn a) -> Maybe (BooleanF vn a)
+simplifyPrimitive = let
     simp2 :: BooleanF vn (BooleanF vn (BooleanExpr vn)) -> BooleanF vn (BooleanExpr vn)
+    simp2 = (liftM2 fromMaybe) (fmap Fix) simp
+    {-
     simp2 e = case simp e of
         Nothing -> fmap Fix e
-        -- Repeating on children is not needed here
-        --Just e  -> myCata2 simp2 $ Fix e    -- repeat application (including to children)
         Just e  -> simp2 $ fmap unFix e
+    -}
+    -- simp2 e = maybe (fmap Fix e) (simp2 . fmap unFix) (simp e)
 
-    simp3 :: Fix (BooleanF vn) -> (BooleanF vn (BooleanExpr vn))
-    simp3 = myCata2 simp2
+    in Fix . (cata simp2)
 
-    simp4 :: BooleanExpr vn -> BooleanExpr vn
-    simp4 = Fix . simp3
+-- simp' :: BooleanF vn (BooleanF vn (BooleanExpr vn)) -> BooleanF vn (BooleanExpr vn)
+-- simp' = (liftM2 fromMaybe) (fmap Fix) simp
 
-    in simp4 e
+-- simplifyPrimitive2 :: BooleanExpr vn -> BooleanExpr vn
+-- simplifyPrimitive2 = Fix . cata simp'
 
 exampleExpr02 :: BooleanExpr String
 exampleExpr02 = bNot ((bNot bTrue) `bAnd` (bNot bFalse))
-
-
-{-
--- FIXME: This is actually just fmap on f-alg
-atChildren :: (BooleanExpr vn -> BooleanExpr vn) -> BooleanExpr vn -> BooleanExpr vn
-atChildren f (BENot e) = BENot (f e)
-atChildren f (BEAnd e1 e2) = BEAnd (f e1) (f e2)
-atChildren f (BEOr e1 e2) = BEOr (f e1) (f e2)
-atChildren _ e = e
-
--- FIXME: This is actually just cata on f-alg
-everywhere :: (BooleanExpr vn -> BooleanExpr vn) -> BooleanExpr vn -> BooleanExpr vn
-everywhere f = f . atChildren (everywhere f)
-
--- Apply function everywhere as long as it doesn't return Nothing
--- This is kinda monadic
-asLongAsPossible :: (BooleanExpr vn -> Maybe (BooleanExpr vn))
-    -> BooleanExpr vn -> BooleanExpr vn
-asLongAsPossible f = everywhere g where
-    g x = maybe x (everywhere g) (f x)
--- becomes: g x = maybe x (cata g) (f x)
-
-simplifyPrimitive :: BooleanExpr vn -> BooleanExpr vn
-simplifyPrimitive = asLongAsPossible simp where
-    simp :: BooleanExpr vn -> Maybe (BooleanExpr vn)
-    simp (BENot (BEVal BTrue)) = Just (BEVal BFalse)
-    simp (BENot (BEVal BFalse)) = Just (BEVal BTrue)
-    simp (BEAnd (BEVal BTrue) e) = Just e
-    simp (BEAnd (BEVal BFalse) _) = Just (BEVal BFalse)
-    simp (BEAnd e (BEVal BTrue)) = Just e
-    simp (BEAnd _ (BEVal BFalse)) = Just (BEVal BFalse)
-    simp (BEOr (BEVal BTrue) _) = Just (BEVal BTrue)
-    simp (BEOr (BEVal BFalse) e) = Just e
-    simp (BEOr _ (BEVal BTrue)) = Just (BEVal BTrue)
-    simp (BEOr e (BEVal BFalse)) = Just e
-    simp _ = Nothing
-
-yes = BEVal BTrue
-no = BEVal BFalse
-
-exampleExpr02 :: BooleanExpr String
-exampleExpr02 = BENot ((BENot yes) `BEAnd` (BENot no))
--}
-
-{-
-simplifyPrimitive e
-=== asLongAsPossible simp e
-=== everywhere g e where g x = maybe x (everywhere g) (simp x)
-    -- IS: cata g e
-=== (g . atChildren (everywhere g)) e where ...     -- simplifies children first
-    -- IS 
-    -- say (atChildren (everywhere g) e) yields e
-=== g e where ...
-=== maybe e (everywhere g) (simp e) where ...       -- simplifies e
-    -- say simp e = Just y
-=== maybe e (everywhere g) (Just y) where ...
-=== everywhere g y where ...                        -- repeats, simplifying root+children
--}
